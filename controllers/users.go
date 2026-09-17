e872f8b34fc44aa4940c9621ef98f522e62b7e01
Paphangkon <paphangkon009@gmail.com>
2026-09-18 00:39:57 +0700

    feat: mock and user error handling controllers

--- controllers/users.go
+++ controllers/users.go
@@ -1,28 +1,74 @@
 package controllers
 
 import (
+	"strconv"
+
+	"github.com/detectivegot/fiber-learn/data"
+	"github.com/detectivegot/fiber-learn/models"
 	"github.com/gofiber/fiber/v3"
 )
 
 func GetUser(c fiber.Ctx) error {
-	id := c.Params("id")
-	return c.SendString("Get one user " + id)
+	idStr := c.Params("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString("Invalid user id")
+	}
+	for i := range data.Users {
+		if data.Users[i].Id==id {
+			return c.JSON(data.Users[i])
+		}
+	}
+	return c.Status(fiber.StatusNotFound).SendString("User " + idStr + " is not found.")
 }
 
 func GetUsers(c fiber.Ctx) error {
-	return c.SendString("Get users")
+	return c.JSON(data.Users)
 }
 
 func CreateUser(c fiber.Ctx) error {
-	return c.SendString("Create user")
+	var user models.Users
+
+	if err := c.Bind().Body(&user); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString("Invaild request.")
+	}
+
+	data.Users = append(data.Users, user)
+	return c.Status(fiber.StatusCreated).JSON(user)
 }
 
 func UpdateUser(c fiber.Ctx) error {
-	id := c.Params("id")
-	return c.SendString("Update user " + id)
+	idStr := c.Params("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString("Invalid user id")
+	}
+	var user models.Users
+	if err := c.Bind().Body(&user); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString("Invaild request.")
+	}
+	for i := range data.Users {
+		if id == data.Users[i].Id {
+			data.Users[i] = user
+			return c.Status(fiber.StatusAccepted).JSON(user)
+		}
+	}
+	return c.Status(fiber.StatusBadRequest).SendString("Invalid user")
 }
 
 func DeleteUser(c fiber.Ctx) error {
-	id := c.Params("id")
-	return c.SendString("Delete user " + id)
+	idStr := c.Params("id")
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString("Invalid user id")
+	}
+	for i := range data.Users {
+		if id == data.Users[i].Id {
+			user := data.Users[i]
+			data.Users = append(data.Users[:i], data.Users[i+1:]...)
+			return c.Status(fiber.StatusAccepted).JSON(user)
+		}
+	}
+	return c.Status(fiber.StatusBadRequest).SendString("Invalid user")
 }
---
+++ data/users.go
@@ -0,0 +1,23 @@
+package data
+
+import (
+	"github.com/detectivegot/fiber-learn/models"
+)
+
+var Users = []models.Users{
+	{
+		Id: 1,
+		Name: "Got",
+		Age: 10,
+	},
+	{
+		Id: 2,
+		Name: "Laplace",
+		Age: 11,
+	},
+	{
+		Id: 3,
+		Name: "Orivia",
+		Age: 13,
+	},
+}
